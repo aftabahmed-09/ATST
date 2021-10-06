6be2b2b2c53907230df1016865de61bdbc4668ef
Fab-c18 <skmaqeel29@gmail.com>
2021-10-06 13:12:31 +0530

    combined test and vehicle count

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import cv2
 import random
+import vehicle_counting
+from PIL import Image
 
 def frames(type,address):
     if type==0:
@@ -12,11 +14,15 @@ def frames(type,address):
             flag,frame=cap.read()
             if flag==False:
                 break    
-            if j%350==0:          
-                cv2.imwrite('luci'+str(i)+'.jpg',frame)
+            if j%350==0:    
+                add='luci'+str(i)+'.jpg'
+                cv2.imwrite(add,frame)
+                img_addr=rf"C:/Users/AQEEL/Desktop/Aqeel/sem-5/MP/new/drive/{add}" 
+                vehicle_counting.count(img_addr)
                 i+=1
+                
             j+=1
     cap.release()
 
-frames(0,r"C:\Users\AQEEL\Desktop\Aqeel\sem-5\MP\sample videos\luci.mp4")
+frames(1,r"C:\Users\AQEEL\Desktop\Aqeel\sem-5\MP\sample videos\luci.mp4")
 cv2.destroyAllWindows()
--- vehicle_counting.py
+++ vehicle_counting.py
@@ -2,31 +2,31 @@ import cv2
 import glob
 from vehicle_detector import VehicleDetector
 
-# Load Veichle Detector
-vd = VehicleDetector()
+def count(img_add):
+    # Load Veichle Detector
+    vd = VehicleDetector()
 
-# Load images from a folder
-img_add= r"C:/Users/AQEEL/Desktop/Aqeel/sem-5/MP/new/drive/images/1.jpg"
-img=cv2.imread(img_add)
+    # Load images from a folder
+    img=cv2.imread(img_add)
 
-vehicles_folder_count = 0
+    vehicles_folder_count = 0
 
-# Loop through all the images
-vehicle_boxes = vd.detect_vehicles(img)
-vehicle_count = len(vehicle_boxes)
+    # Loop through all the images
+    vehicle_boxes = vd.detect_vehicles(img)
+    vehicle_count = len(vehicle_boxes)
 
-    # Update total count
-vehicles_folder_count += vehicle_count
+        # Update total count
+    vehicles_folder_count += vehicle_count
 
-for box in vehicle_boxes:
-    x, y, w, h = box
+    for box in vehicle_boxes:
+        x, y, w, h = box
 
-    cv2.rectangle(img, (x, y), (x + w, y + h), (25, 0, 180), 3)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (25, 0, 180), 3)
 
-    cv2.putText(img, "Vehicles: " + str(vehicle_count), (20, 50), 0, 2, (100, 200, 0), 3)
+        cv2.putText(img, "Vehicles: " + str(vehicle_count), (20, 50), 0, 2, (100, 200, 0), 3)
 
-cv2.imshow("Cars", img)
-cv2.waitKey(0)
+    cv2.imshow("Cars", img)
+    cv2.waitKey(0)
 
-print("Total current count", vehicles_folder_count)
+    print("Total current count", vehicles_folder_count)
 
