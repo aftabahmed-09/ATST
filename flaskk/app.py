018f40ad178bd28fa1e451d67534146da01b8440
Fab-c18 <71333590+Fab-c18@users.noreply.github.com>
2021-11-11 10:01:19 +0530

    Add files via upload

--- flaskk/app.py
+++ flaskk/app.py
@@ -4,23 +4,37 @@ from flask import Flask , render_template ,request,Response
 import vehicle_counting
 import cv2
 
-UPLOAD_FOLDER = r'C:\Users\AQEEL\Desktop\Aqeel\sem-5\MP\drive\flask\test\fol'
+UPLOAD_FOLDER = r'C:\Users\AQEEL\Desktop\Aqeel\sem-5\MP\flaskkkkk\test\flaskk\fol'
 
-
+vidpath=''
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET','POST'])
+def home():
+    return render_template('index1.html')
+@app.route('/v', methods=['GET','POST'])
 def hello_world():
-    return render_template('index.html')
+    return render_template('vidcap.html')
+
+@app.route('/vid',methods=['GET','POST'])
+def path():
+    global vidpath
+    vidfile=request.files['vidfile']
+    filename = secure_filename(vidfile.filename)
+    vidpath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    vidfile.save(vidpath)
+    return render_template('display2.html')
 @app.route('/display', methods=['GET'])
 def hello():
     return render_template('display.html')
 
 @app.route('/p', methods=['POST','GET'])
-def frames():
-    
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+def frames(type,address):
+    if type==0:
+        cap=cv2.VideoCapture(address)
+    if type==1:
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     i=0
     j=0
     while(cap.isOpened()):
@@ -30,7 +44,7 @@ def frames():
             if j%30==0:    
                 add='luci'+str(i)+'.jpg'
                 cv2.imwrite(add,frame)
-                img_addr=rf"C:/Users/AQEEL/Desktop/Aqeel/sem-5/MP/flaskkkkk/{add}" 
+                img_addr=rf"C:/Users/AQEEL/Desktop/Aqeel/sem-5/MP/flaskkkkk/test/{add}" 
                 vehicle_counting.count(img_addr)
                 i+=1
                 img = cv2.imread(img_addr)
@@ -40,9 +54,13 @@ def frames():
                 
             j+=1
     cap.release()
+@app.route('/live_feed')
+def live_feed():
+    return Response(frames(1,0), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/video_feed')
 def video_feed():
-    return Response(frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    print(vidpath)
+    return Response(frames(0,vidpath), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
     app.run(debug='true')
